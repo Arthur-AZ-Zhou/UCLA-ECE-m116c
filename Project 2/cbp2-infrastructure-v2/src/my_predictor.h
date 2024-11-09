903967089787fbfb833eb5f816786569d53adaf4
Arthur Zhou <arthuracct@gmail.com>
2024-11-08 19:46:15 -0800

    project 2 done - made the branch table more efficient

--- Project 2/cbp2-infrastructure-v2/src/my_predictor.h
+++ Project 2/cbp2-infrastructure-v2/src/my_predictor.h
@@ -4,6 +4,9 @@
 // Note that this predictor doesn't use the whole 32 kilobytes available
 // for the CBP-2 contest; it is just an example.
 
+#include <algorithm>
+using namespace std;
+
 class my_update : public branch_update {
 public:
 	unsigned int index;
@@ -11,15 +14,25 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	15
-#define TABLE_BITS	15
+#define HISTORY_LENGTH	21 //changed this so we can maintain a longer branch history
+#define TABLE_BITS	21 //look at above comment
+#define BYTE_COUNTERS 4
 	my_update u;
 	branch_info bi;
 	unsigned int history;
 	unsigned char tab[1<<TABLE_BITS];
 
 	my_predictor (void) : history(0) { 
-		memset (tab, 0, sizeof (tab));
+		fill(tab, tab + (1 << TABLE_BITS), 0); //CHANGED TO FILL BECAUSE IT IS FASTER THAN MEMSET
+	}
+
+	unsigned char readCounter(unsigned int index) { //adjust frequency based on this
+		return (tab[index / BYTE_COUNTERS] >> ((index % BYTE_COUNTERS) * 2)) & 0x3; 
+	}
+
+	void writeCounter(unsigned int index, unsigned char val) { //adjust frequency based on this
+		tab[index / BYTE_COUNTERS] &= ~(0x3 << ((index % BYTE_COUNTERS) * 2));
+		tab[index / BYTE_COUNTERS] |= (val & 0x3) << ((index % BYTE_COUNTERS) * 2);
 	}
 
 	branch_update *predict (branch_info & b) {
@@ -28,7 +41,7 @@ public:
 			u.index = 
 				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
 				^ (b.address & ((1<<TABLE_BITS)-1));
-			u.direction_prediction (tab[u.index] >> 1);
+			u.direction_prediction (readCounter(u.index) >> 1);
 		} else {
 			u.direction_prediction (true);
 		}
@@ -38,15 +51,14 @@ public:
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {
-			unsigned char *c = &tab[((my_update*)u)->index];
+			unsigned char c = readCounter(((my_update*)u)->index);
 			if (taken) {
-				if (*c < 3) (*c)++;
+				if (c < 3) (c)++;
 			} else {
-				if (*c > 0) (*c)--;
+				if (c > 0) (c)--;
 			}
-			history <<= 1;
-			history |= taken;
-			history &= (1<<HISTORY_LENGTH)-1;
+			writeCounter(((my_update*)u)->index, c);
+			history = ((history << 1) | taken) & ((1 << HISTORY_LENGTH) - 1);
 		}
 	}
 };
